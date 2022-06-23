5ebe6576b280245a02ef99c7f7a87c802c27f6fb
unknown <m.cristivrabie@yahoo.ro>
2022-06-23 10:14:47 +0300

    small app for testing

---
+++ src/pages/TaskList/TaskList.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import './TaskList.scss'
+
+export const TaskList = () => {
+    const [isAddingTask, setIsAddingTask] = useState(false);
+    const [taskName, setTaskName] = useState('a');
+    const [description, setDescription] = useState('b');
+    const [taskList, setTaskList] = useState<{name:string, description:string}[]>([])
+
+    const handleSaveTask = () => {
+        if(!taskName || !description){
+            return;
+        }
+        
+        const list = [...taskList];
+        list.push({
+            name: taskName,
+            description:description
+        })
+        setTaskList(list);
+        setTaskName('');
+        setDescription('');
+        setIsAddingTask(false);
+    }
+
+    return (
+        <div className='task-list'>
+            <div className='task-list__name'>Tasks list</div>
+            <button className='task-list__button task-list__button--add' onClick={() => setIsAddingTask(!isAddingTask)} > Add </button>
+
+            {isAddingTask && (
+                <div className='task-list__add-task'>
+                    <input className='task-list__input' value={taskName} onChange={(e) => setTaskName(e.target.value)} />
+                    <input className='task-list__input' value={description} onChange={(e) => setDescription(e.target.value)} />
+                    <button className='task-list__button task-list__button--save' onClick={handleSaveTask}>Save</button>
+                </div>
+            )}
+
+            <ul>
+                {taskList.map((task:any) => (
+                    <li>
+                        <span className='task-list__task-name'>{task.name}</span> -
+                        <span className='task-list__task-description'>{task.description}</span>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
